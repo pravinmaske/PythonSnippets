8ab3fbe8ab50c1aa4ead8a7afa29c5bd7df27cf9
pravinmaske963@gmail.com <pravinmaske963@gmail.com>
2018-01-19 22:29:43 +0530

    For loop Types demo

---
+++ For_Loop_Demo.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 30 20:24:32 2017
+
+@author: PRAVIN
+"""
+
+for i in[1,2,3]:
+    print(i)
+    
+for ch in "Pravin!":
+    print(ch)
+    
+for i in range(5):
+    print("Yes you will defenitely Product base company")
+    
+
+for i in range(5):
+    print("I'm the best")
+
+for char in "Maske":
+    print(char)
+    '''String is an Sequence in Python and Sequence is an Orderd collection of Objects/'''
+    
+for k in range(6):
+    print("k six") 
+for i in range(5):
+    print(i*i)    
+    
+    
