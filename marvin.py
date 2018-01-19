ae0514886f3734dae81c7435e2c07f0dfa043bde
pravinmaske963@gmail.com <pravinmaske963@gmail.com>
2018-01-19 22:46:35 +0530

    Marvin for loop slice demo

---
+++ marvin.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 19 19:47:24 2018
+
+@author: PRAVIN
+"""
+
+paranoid_android="Marvin"
+letters=list(paranoid_android)
+for char in letters:
+    print("\t",char)
