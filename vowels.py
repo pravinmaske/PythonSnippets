44133a5c3daa6b3b186b99f28cab742ed89d2d65
pravinmaske963@gmail.com <pravinmaske963@gmail.com>
2018-01-19 22:39:53 +0530

    vowels List demo

---
+++ vowels.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 12 19:44:56 2018
+
+@author: PRAVIN
+"""
+            
+vowels=['a','e','i','o','u']
+word="Milliways"
+for letter in word:
+    if letter in vowels:
+        print(letter)
+        
+            
+        
+        
